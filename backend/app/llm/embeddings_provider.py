d44b658296b670c95a6f02dc5be225d001e4a13e
Basantkhanal07 <me.basantkhanal07@gmail.com>
2026-02-16 12:38:59 +0545

    change

--- backend/app/llm/embeddings_provider.py
+++ backend/app/llm/embeddings_provider.py
@@ -1,5 +1,5 @@
 # Importing Embeddings model from LangChain_huggingface
-from langchain_huggingface import HuggingFaceEmbeddings
+#from langchain_huggingface import HuggingFaceEmbeddings
 
 # Initializing embeddings model
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
